db6c7af2076a01c523b69d27e8f6e63603f3297f
Jing060206 <109713103+Jing060206@users.noreply.github.com>
2023-05-12 14:51:06 -0500

    update

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const API = (() => {
   const URL = "http://localhost:3000";
+
   const getCart = () => {
     // define your method to get cart data
+    fetch(URL + "/cart").then((data) => data.json());
   };
 
-  const getInventory = () => {
-    // define your method to get inventory data
-  };
+  // define your method to get inventory data
+
+  const getInventory = () =>
+    fetch(URL + "/inventory").then((data) => data.json());
 
   const addToCart = (inventoryItem) => {
     // define your method to add an item to cart
+    return fetch(URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(inventoryItem),
+    }).then((res) => res.json());
   };
 
   const updateCart = (id, newAmount) => {
     // define your method to update an item in cart
+    return fetch(URL + `/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(updateTodo),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => res.json());
   };
 
   const deleteFromCart = (id) => {
     // define your method to delete an item in cart
+    return fetch(URL + `/${id}`, {
+      method: "DELETE",
+    }).then((res) => res.json());
   };
 
   const checkout = () => {
@@ -55,10 +71,18 @@ const Model = (() => {
       return this.#inventory;
     }
 
-    set cart(newCart) {}
-    set inventory(newInventory) {}
+    set cart(newCart) {
+      this.#cart = newCart;
+      this.#onChange?.();
+    }
+    set inventory(newInventory) {
+      this.#inventory = newInventory;
+      this.#onChange?.();
+    }
 
-    subscribe(cb) {}
+    subscribe(cb) {
+      this.#onChange = cb;
+    }
   }
   const {
     getCart,
@@ -81,22 +105,81 @@ const Model = (() => {
 
 const View = (() => {
   // implement your logic for View
-  return {};
+  const inventoryListEl = document.querySelector(".inventory-container ul");
+  const cartListEl = document.querySelector(".cart-wrapper ul");
+  const decreaseBtn = document.getElementById("increase");
+
+  const renderInventory = (inventory) => {
+    let inventoryTemp = "";
+    inventory.forEach((item) => {
+      const liTemp = `
+          <li id="${item.id}">
+          <span>${item.content}</span>
+          <button class="decrease-btn">-</button>
+          <span id="amount" class="amount">9</span>
+          <button id="increase"class="increase-btn">+</button>
+          <button class="add-cart-btn">add to cart</button>
+          </li>
+          `;
+      inventoryTemp += liTemp;
+    });
+
+    inventoryListEl.innerHTML = inventoryTemp;
+  };
+
+  return {
+    inventoryListEl,
+    cartListEl,
+    decreaseBtn,
+    renderInventory,
+  };
 })();
 
 const Controller = ((model, view) => {
   // implement your logic for Controller
   const state = new model.State();
 
-  const init = () => {};
-  const handleUpdateAmount = () => {};
+  const init = () => {
+    model.getInventory().then((data) => {
+      state.inventory = data;
+    });
+  };
+  const handleUpdateAmount = (event) => {
+    view.inventoryListEl.addEventListener("click", (event) => {
+      let amountEl = document.getElementById("amount");
+      let quantity = parseInt(amountEl.innerText);
+
+      if (event.target.className !== "decrease-btn") return;
+
+      if (event.target.className === "decrease-btn") {
+        if (quantity > 0) {
+          quantity--;
+          amountEl.innerText = quantity;
+        }
+      }
+
+      if (event.target.className !== "increase-btn") return;
+      if (event.target.className === "increase-btn") {
+        quantity++;
+        amountEl.innerText = quantity;
+      }
+    });
+  };
 
   const handleAddToCart = () => {};
 
   const handleDelete = () => {};
 
   const handleCheckout = () => {};
-  const bootstrap = () => {};
+  const bootstrap = () => {
+    init();
+    state.subscribe(() => {
+      view.renderInventory(state.inventory);
+    });
+    handleUpdateAmount();
+
+    handleCheckout();
+  };
   return {
     bootstrap,
   };
